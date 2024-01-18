3454fcb8d9ebc9af7b72b2ff37961656927d3d8d
naka ryunosuke <naka.sava0309@gmail.com>
2024-01-18 20:15:42 +0900

    Add: データの追加

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,9 +1,14 @@
-# class Result < ApplicationRecord
-# end
-
 class Result < ActiveHash::Base
     self.data = [
-      { id: 1, name: "フライングピカチュウ", image: 'flying_pikachu.jpeg' },
-      { id: 2, name: "フシギバナ", image: 'fushigibana.PNG' }
+        { id: 1, name: "フライングピカチュウ", image: 'flying_pikachu.jpeg' },
+        { id: 2, name: "フシギバナ", image: 'fushigibana.PNG' },
+        { id: 3, name: "登山中に捻挫した人", image: 'tozan.jpeg' },
+        { id: 4, name: "みかんを食べながらこたつでテレビを観ている虎", image: 'mikantora.jpeg' },
+        #   { id: , name: "", image: '' },
+        #   { id: , name: "", image: '' },
+        #   { id: , name: "", image: '' },
+        #   { id: , name: "", image: '' },
+        #   { id: , name: "", image: '' },
+        #   { id: , name: "", image: '' }
     ]
 end
