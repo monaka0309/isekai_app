739d49c8bf79176bd3909e222413c1ae50e9983a
naka ryunosuke <naka.sava0309@gmail.com>
2024-01-19 11:02:54 +0900

    Add: データの追加

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,17 +1,14 @@
 class Result < ActiveHash::Base
     self.data = [
         { id: 1, name: "フライングピカチュウ", image: 'flying_pikachu.jpeg' },
-        # { id: 2, name: "ただのフシギバナ", image: 'fushigibana.PNG' },
-        # { id: 2, name: "首がつまったキリン", image: '' },
+        { id: 2, name: "首がつまった2匹のキリン", image: 'kirin.jpeg' },
         { id: 3, name: "登山中に捻挫した人", image: 'tozan.jpeg' },
         { id: 4, name: "みかんを食べながらこたつでテレビを観ている虎", image: 'mikantora.jpeg' },
         { id: 5, name: "髭が長めの16歳の勇者", image: '16hige.jpeg' },
         { id: 6, name: "ライオンを夢見るカエル", image: 'kaeru.jpeg' },
-        #   { id: 7, name: "フシギバナになりたい女の子", image: '' },
-        #   { id: 8, name: "ギロチン台に立っている貴族", image: '' },
-        #   { id: 9, name: "むきむきになったバッタ", image: '' },
-        #   { id: 10, name: "セクシーすぎるアリクイ", image: '' },
-        #   { id: 11, name: "絶頂を迎えたカービィー", image: '' }
-        #   { id: 12, name: "", image: '' }
+        { id: 7, name: "麻雀を楽しむ総理大臣", image: 'souri.jpeg' },
+        { id: 8, name: "ギロチン台に立っている貴族", image: 'girotin.jpeg' },
+        { id: 9, name: "むきむきになった亀", image: 'kame.jpeg' },
+        { id: 10, name: "落ちてる歯を吸い込んだカービィー", image: 'kabi.jpeg' }
     ]
 end
